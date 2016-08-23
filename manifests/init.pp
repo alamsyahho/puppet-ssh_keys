4bf0312a813c6bbad1cb6918ec1c9821cb81bedd
Alamsyah <alamsyah@incube8.sg>
2016-08-23 15:07:54 +0800

    Enable hiera config support on ssh keys

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,4 +5,10 @@ class ssh_keys {
     undef   => $ssh_keys::params::fallback_homedir_location,
     default => $_homedir_base
   }
+
+  $user_hash = hiera('ssh_keys::user', {})
+  create_resources('ssh_keys::user', $user_hash)
+
+  $authorized_key_hash = hiera('ssh_keys::authorized_key', {})
+  create_resources('ssh_keys::authorized_key', $authorized_key_hash)
 }
